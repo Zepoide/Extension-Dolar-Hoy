617959d553ff5cf49c8a536e307e77949ffdb2e5
Zepoide <santi.tamarit07@gmail.com>
2023-08-22 16:16:06 -0300

    .

--- main.js
+++ main.js
@@ -2,9 +2,11 @@
 
 const htmlVenta = document.getElementById("venta")
 const htmlCompra = document.getElementById("compra")
+// const htmlIcono = document.getElementById("sube-baja")
+
+// let valor_compra_old = null
 
 async function fetchValorDolar(){
-    
     const response = await fetch("https://api.bluelytics.com.ar/v2/latest")
     const json = await response.json()
     const valor_compra = json.blue.value_buy
@@ -12,7 +14,16 @@ async function fetchValorDolar(){
 
     htmlCompra.innerHTML = `$${valor_compra}`
     htmlVenta.innerHTML = `$${valor_venta}`
+
 }
 
 fetchValorDolar()
 
+
+// if (valor_compra > valor_compra_old){
+//     htmlIcono.innerHTML = "&#8682"
+// }else if (valor_compra === valor_compra_old){
+//     htmlIcono.innerHTML = "&#8776"
+// }else{
+//     htmlIcono.innerHTML = "&#8659"
+// }
